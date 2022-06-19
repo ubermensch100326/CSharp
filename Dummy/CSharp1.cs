436bd8db7b2526af54544a71492efa736fd022dc
Hojo Choi <ubermensch100326@gmail.com>
2022-06-19 15:39:48 +0900

    commit

---
+++ Dummy/CSharp1.cs
@@ -0,0 +1,13 @@
+/*
+1. IDE (Integrated Development Environment; 통합개발환경)
+코드를 치자마자 결과나 내가 잘못한 부분을 알려줌
+내가 어떤 일을 할 수 있는지 사전 외우듯이 공부를 하지 않아도 됨
+프리젠테이션을 도와주는 파워포인트와 유사하게 프로그래밍을 도와주는 프로그램
+IDE에는 visual studio 뿐만 아니라 이클립스 등 다양하게 존재함
+
+
+2. 일반적으로 C#은 visual studio를 사용
+
+
+3. dotnet new console -o TestProject
+*/
---
+++ Dummy/CSharp2.cs
@@ -0,0 +1,10 @@
+namespace Charp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕하세요 동아리 여러분");
+        }
+    }
+}
